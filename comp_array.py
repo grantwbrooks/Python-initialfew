1a79842db6eac175d9df345d041587742e58b8f1
grantwbrooks <grantwbrooks@gmail.com>
2017-07-31 22:43:51 -0400

    finished compare arrays

---
+++ comp_array.py
@@ -0,0 +1,8 @@
+list_one = [1,2,5,6,5]
+list_two = [1,2,5,6,5,3]
+
+if list_one == list_two:
+    print "The lists are the same"
+
+elif list_one != list_two:
+    print "The lists are not the same."
